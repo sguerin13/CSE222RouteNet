943a9a126f46e83d9ac79fb5f07c2161b5d11611
Steve Guerin <sguerinbb@gmail.com>
2021-03-13 14:39:24 -0500

    updated training routine

--- model.py
+++ model.py
@@ -1,11 +1,8 @@
-import Runner
 import torch
 import torch.nn as nn
-from torch_geometric.data import DataLoader
-from torch_geometric.nn import GCNConv
+import torch.nn.functional as F
 
 '''using the implementation from RouteNet'''
-
 class RouteNet(nn.Module):
 
     def __init__(self):
@@ -16,6 +13,7 @@ class RouteNet(nn.Module):
         # (batch, seq_len, feature size) or seq_len, batch, feature size
         # if sequence length is variable
         # may need to pad the sequence
+        
         self.link_state_dim = 32
         self.path_state_dim = 32
         self.readout_dim = 8
@@ -27,98 +25,152 @@ class RouteNet(nn.Module):
         readSize = 10
         nLayer = 1
 
-        # need to dial this in a bit
-        self.l_U = nn.GRU(self.link_state_dim,hSize,nLayer,batch_first=True)
-        self.p_U = nn.GRU(self.path_state_dim,hSize,nLayer,batch_first=True)
-
-        self.readOut1 = nn.Linear(hSize,self.readout_dim)
-        self.readOut2 = nn.Linear(self.readout_dim,self.readout_dim)
-        self.readOut3 = nn.Linear(self.readout_dim,self.output_units)
-
+        self.l_U = nn.GRU(input_size = self.link_state_dim,
+                          hidden_size = hSize,
+                          num_layers = nLayer,
+                          batch_first=True)
+        
+        self.p_U = nn.GRU(input_size = self.path_state_dim,
+                          hidden_size = hSize,
+                          num_layers = nLayer,
+                          batch_first=True)
+        
+        self.readOut = nn.ModuleDict({  'r1': nn.Linear(hSize,self.readout_dim),
+                                        'r2': nn.Linear(self.readout_dim,self.readout_dim),
+                                        'r3': nn.Linear(self.readout_dim,self.output_units)
+                                        })
+        
 
     def forward(self,x):
+        
         links = x['links']
         paths = x['paths']
-        seqs = x['sequences']
+        seqs  = x['sequences']
+        bandwidth = x['bandwith']
+        link_cap  = x['link_capacity']
 
         # state matrix shape for the link
-        link_h_state_shape = torch.stack([x['n_links'], self.link_state_dim-1], axis = 1)
+        link_h_state_shape = (x['n_links'], self.link_state_dim-1)
 
-        # create hidden state matrix for links and initialize with first column as link capacity
-        link_state = torch.cat([torch.unsqueeze(x['link_capacity'],axis=1),
-                                torch.zeros(link_h_state_shape)])
-
-        # state matrix shape for the path  
-        path_h_state_shape = torch.stack([ x['n_paths'],self.path_state_dim-1], axis=0)
-
-        # create hidden state matrix for path and initialize with first column as path bandwith
-        path_state = torch.cat([torch.unsqueeze(x['bandwith'], axis=1),
-                                torch.zeros(path_h_state_shape)], axis=1)
-
-        for t in range(self.T):
-
-            # flatten the double loop into a bulk matrix using the gather functionality
-            # this is an aggregation of the state vector of each link on each path flattened into a (sum_paths(sum_links_on_paths) x h_state) size matrix 
-            # https://medium.com/analytics-vidhya/understanding-indexing-with-pytorch-gather-33717a84ebc4
-            indices = torch.zeros(len(links),32)
-            for i in range(len(links)):
-                link_id = links[i]
-                indices[i,:] = link_id
-
-            h_t_links_on_paths = torch.gather(link_h_state,0,indices.long())
-
-            #stack the paths and sequences
-            paths = torch.tensor(paths)
-            seqs = torch.tensor(seqs)
-            ids = torch.stack((paths,seqs),axis=1)
-
-            # prepare input for path update RNN
-            max_seq_len = torch.max(seqs)
-            path_rnn_input_shape = (x['n_paths'],max_seq_len,self.link_state_dim)
-
-            # aggregate the sequence lengths
-            scatter_src = torch.unsqueeze(torch.ones_like(paths),1)
-            lens = torch.zeros(x['n_paths'],1).long()
-            _ = lens.scatter_add_(dim = 0, src = scatter_src,
-                           index=torch.unsqueeze(paths,1))
-
-            # move the elements from h_t_links on paths to the rnn input
-            path_rnn_input = torch.zeros(path_rnn_input_shape)
-            for i in range(ids.shape[0]):
-                p_ind = ids[i,0]
-                s_ind = ids[i,1]
-                path_rnn_input[p_ind,s_ind,:] = h_t_links_on_paths[i]
-
+        # create hidden state matrix shape for the path  
+        path_h_state_shape = (x['n_paths'],self.path_state_dim-1)
+        path_h_state = torch.cat((bandwidth,torch.zeros(path_h_state_shape)), axis=1)
         
-            hidden_states, path_state = self.P_U(path_rnn_input,initial_state)
+        # prepare input for path update RNN
+        max_seq_len = torch.max(seqs)
+        path_rnn_input_shape = (x['n_paths'],max_seq_len+1,self.link_state_dim)
+        
+        #stack the paths and sequences
+        ids = torch.stack((paths,seqs),axis=1)
+        ids = torch.squeeze(ids,2)           
+        p_ind = ids[:,0]
+        s_ind = ids[:,1]
 
-            # For every link, gather the input from the hidden states of the paths
-            # outputs is size (n_path, max_seq, link_state_dim)  182 x 3 x 32
-            # ids are size (sum_path(sum_link_in_path) x 2)      390 x 2
-            # pytorch doesn't have a gather_nd instead it will grab by output
-            link_messages = torch.zeros((len(links),self.link_state_dim))
-            for i in range(ids.shape[0]):
-                p_ind = ids[i,0]
-                s_ind = ids[i,1]
-                link_messages[i] = hidden_states[p_ind,s_ind,:]
+        # flatten the double loop into a bulk matrix using the gather functionality
+        # this is an aggregation of the state vector of each link on each path flattened into a (sum_paths(sum_links_on_paths) x h_state) size matrix 
+        # using torch.gather
+        indices = torch.zeros(len(links),32)
+        for i in range(len(links)):
+            link_id = links[i]
+            indices[i,:] = link_id
+            
+        # variable dictionary for forward pass
+        vd = {}
+        
+        for t in range(self.T):
+            
+            
+            ############# set up the matrices and variables for each pass through #################
+            
+            ########## PATH VARIABLES ###########
+            
+            # input to the path rnn layer P_u
+            path_rnn_input_key = 'path_rnn_input_' + str(t)
+            vd[path_rnn_input_key] = torch.zeros(path_rnn_input_shape)
+            
+            if (t > 0):  # for non leaf variables, we need to propagate the gradient back
+                vd[path_rnn_input_key].requires_grad = True
+            
+            
+            # path hidden state output from P_U, initialized with just bandwidth at T_0, else copy
+            path_h_state_key = 'path_h_state_' + str(t)
+            if (t==0):
+                vd[path_h_state_key] = torch.cat((bandwidth,torch.zeros(path_h_state_shape)), axis=1)
+            else:
+                path_h_state_key = 'path_h_state_' + str(t)
+                path_h_prev = 'path_h_state_' + str(t-1)
+                vd[path_h_state_key] = vd[path_h_prev]
+            
+            
+            # path_hidden state sequence from P_U, used to update links
+            path_h_state_seq_key = 'path_h_states_seq_' + str(t)
+            
+            
+            ########## LINK VARIABLES ###########
+            
+            # vector to store the link_hidden states
+            if (t == 0):
+                # create hidden state matrix for links and initialize with first column as link capacity
+                link_h_state_key = 'link_h_state_' + str(t)
+                vd[link_h_state_key] = torch.cat((link_cap,torch.zeros(link_h_state_shape)),1)
+            else:
+                # copy hidden state value for next pass through
+                link_h_state_key = 'link_h_state_' + str(t)
+                linK_h_prev = 'link_h_state_' + str(t-1)
+                vd[link_h_state_key] = vd[linK_h_prev]
+                
+                
+            # matrix storing the hidden states of links on paths
+            # i.e. the hidden state of all links in the x['links'] list
+            h_link_path_key = 'h_links_on_paths_' + str(t)
+            vd[h_link_path_key] = torch.gather(vd[link_h_state_key],0,indices.long())
+            
+            #link messages extracted from the path hidden state sequence output from P_U
+            link_message_key = 'link_messages_' + str(t)
 
-            # aggregate the messages
-            # aggregated link message is size (n_links x link state dim)
-            # aggregated along rows based on link index in the links vector
-            aggregated_link_message = torch.zeros((x['n_links'],link_state_dim))
-            _ = aggregated_link_message.scatter_add_(0,torch.tensor(links).unsqueeze(1),link_messages)
-
-            # update the state of the links
-            link_state, _ = self.l_U(aggregated_link_message, [link_state])
-
+            
+             # container for the link messages that are extracted from path rnn hidden states
+            agg_link_message_key = 'aggregated_link_message_' + str(t)
+            vd[agg_link_message_key] = torch.zeros((x['n_links'],self.link_state_dim),requires_grad=True)
+            
+            ########################################################################################
+            
+            
+            ################################## DO THE MESSAGE PASSING ##############################
+            
+            # prepare input for path RNN
+            vd[path_rnn_input_key] = vd[path_rnn_input_key].index_put(indices = [p_ind,s_ind],
+                                                                      values = vd[h_link_path_key])
+            # pass through the path RNN
+            vd[path_h_state_seq_key], vd[path_h_state_key] = self.p_U(vd[path_rnn_input_key],
+                                                                      torch.unsqueeze(vd[path_h_state_key],
+                                                                      0))
+            # reformat
+            vd[path_h_state_key] = vd[path_h_state_key].squeeze(0)
+            
+            # extract link messages from the path RNN sequence output
+            # equivalent to tf.gather_nd
+            vd[link_message_key] = vd[path_h_state_seq_key][p_ind,s_ind,:]
+           
+            # aggregate the link messages
+            vd[agg_link_message_key] = vd[agg_link_message_key].index_put([links.squeeze(1)],
+                                                                           vd[link_message_key],
+                                                                           accumulate=True)
+            # update the state of the links by passing through link 
+            _, vd[link_h_state_key] = self.l_U(torch.unsqueeze(vd[agg_link_message_key],1),
+                                               torch.unsqueeze(vd[link_h_state_key].squeeze(0),0))
+            # reformat
+            vd[link_h_state_key] = vd[link_h_state_key].squeeze(0)
+            
+            ##########################################################################################
 
         # readout from the paths
-        y = self.readout(path_state)
+        y = self.readout(vd[path_h_state_key])
         return y
 
-    def readout(self,path_state):
-        x = nn.ReLU(self.readOut1(path_state))
-        x = nn.ReLU(self.readOut2(x))
-        x = self.readOut3(x)
-        return x
 
+    def readout(self,path_state):
+        x = F.relu(self.readOut['r1'](path_state))
+        x = F.relu(self.readOut['r2'](x))
+        x = self.readOut['r3'](x)
+        return x
---
+++ train.py
@@ -0,0 +1,6 @@
+import torch
+from model import RouteNet
+from Runner import Runner
+from read_dataset import  NetworkDataset
+
+
